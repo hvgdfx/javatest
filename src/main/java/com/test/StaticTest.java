39209b3ce5094e0d437686f21bf254120825c047
吴彦祖 <zhanggx@ifeng.com>
2019-08-01 15:11:47 +0800

    add stringtest and statictest

---
+++ src/main/java/com/test/StaticTest.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class StaticTest {
+
+    static {
+        System.out.println("static code...");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/test/StringTest.java
@@ -0,0 +1,21 @@
+package com.test;
+
+public class StringTest {
+
+    public static void main(String[] args) {
+
+        StringBuffer sBuffer = new StringBuffer("你好");
+
+        sBuffer.append("www");
+        sBuffer.append(".baidu");
+        sBuffer.append(".com");
+
+        System.out.println(sBuffer);
+
+        StringBuilder sBuilder = new StringBuilder("hello");
+        sBuilder.append("1");
+        sBuilder.append("2");
+        sBuilder.append("3");
+        System.out.println(sBuilder);
+    }
+}
